5a76d666f4e0967ba9ebcdb3b73c242a9c349b50
Sotatek-SonNguyen16 <son.nguyen16@sotatek.com>
2024-08-05 17:37:50 +0700

    fix: fix can not load asset files

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,42 +1,41 @@
-import {createBrowserRouter, Navigate} from "react-router-dom";
-import AppLayout from "@/components/layout/AppLayout.tsx";
-import {lazy} from "react";
+import { createBrowserRouter, Navigate } from 'react-router-dom'
+import AppLayout from '@/components/layout/AppLayout.tsx'
+import { lazy } from 'react'
 
-const DashboardPage = lazy(() => import('@/views/dashboard/DashboardPage.tsx'));
-const EventPage = lazy(() => import('@/views/event/EventPage.tsx'));
-const MarketPage = lazy(() => import('@/views/market/MarketPage.tsx'));
-const NotMatch = lazy(() => import('@/views/404/NotMatch.tsx'));
+const DashboardPage = lazy(() => import('@/views/dashboard/DashboardPage.tsx'))
+const EventPage = lazy(() => import('@/views/event/EventPage.tsx'))
+const MarketPage = lazy(() => import('@/views/market/MarketPage.tsx'))
+const NotMatch = lazy(() => import('@/views/404/NotMatch.tsx'))
 
 export const router = createBrowserRouter([
     {
-        path: "",
-        element: <Navigate to={`/markets`}/>,
+        path: '',
+        element: <Navigate to={`/markets`} />
     },
     {
-        path: "/dashboard",
-        element: <DashboardPage/>,
+        path: '/dashboard',
+        element: <DashboardPage />
     },
     {
-        path: "",
-        element: <AppLayout/>,
+        path: '',
+        element: <AppLayout />,
         children: [
             {
-                path: "event",
-                element: <EventPage/>,
-            },{
-                path: "event/:id",
-                element: <EventPage/>,
+                path: 'event',
+                element: <Navigate to={`/markets`} />
             },
             {
-                path: "markets",
-                element: <MarketPage/>,
+                path: 'event/:id',
+                element: <EventPage />
             },
-        ],
+            {
+                path: 'markets',
+                element: <MarketPage />
+            }
+        ]
     },
     {
-        path: "*",
-        element: <NotMatch/>,
-    },
-], {
-    basename: global.basename
-})
+        path: '*',
+        element: <NotMatch />
+    }
+])
