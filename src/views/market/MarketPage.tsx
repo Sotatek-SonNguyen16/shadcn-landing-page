4d7add83cf12464caf33efc144f94a72ac8e4f53
Sotatek-SonNguyen16 <son.nguyen16@sotatek.com>
2024-08-09 11:18:28 +0700

    refator: resize padding y

--- src/views/market/MarketPage.tsx
+++ src/views/market/MarketPage.tsx
@@ -5,7 +5,7 @@ import MarketListLayout from '@/views/market/MarketListLayout.tsx'
 const MarketPage: React.FC = () => {
     return (
         <MarketProvider>
-            <div className='container px-4 md:px-8 w-full mt-3'>
+            <div className='container px-4 md:px-8 w-full my-2 lg:my-3'>
                 <div className='mb-3'>{/*<PolyMarketCategoryTabs />*/}</div>
                 <MarketListLayout layout='grid' />
             </div>
