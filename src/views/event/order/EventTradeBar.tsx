d00a84965f1c8e109d0ea91e593f5b4cf33aacb1
Sotatek-SonNguyen16 <son.nguyen16@sotatek.com>
2024-08-15 11:04:14 +0700

    feat: add oposite active order tooltip

--- src/views/event/order/EventTradeBar.tsx
+++ src/views/event/order/EventTradeBar.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from 'react'
 import { clsx } from 'clsx'
 import { Badge } from '@/components/ui/badge.tsx'
-import { EBetOption, EFormType, Order } from '@/types'
+import { EFormType, Order } from '@/types'
 import { useDrawerContext } from '@/contexts/DrawerContext.tsx'
 import SaleDrawer from '@/views/event/SaleDrawer.tsx'
 import useScreenSize from '@/hooks/useScreenSize.ts'
@@ -19,13 +19,7 @@ interface OrderWithTotal extends Order {
 
 const EventTradeBar: React.FC<EventTradeBarProps> = React.memo((props) => {
     const { variant, data } = props
-    const {
-        handleSelectOrder,
-        changeType,
-        activeOrders,
-        betOption,
-        currentMarket
-    } = useEventContext()
+    const { handleSelectOrder, changeType, activeOrders } = useEventContext()
     const { openDrawer } = useDrawerContext()
     const { isLargerThan } = useScreenSize()
 
@@ -113,15 +107,12 @@ const EventTradeBar: React.FC<EventTradeBarProps> = React.memo((props) => {
                         const width = Math.round(
                             (Number(total) / maxTotal) * 100 * 3
                         )
-
                         const activeOrder =
                             activeOrders?.find(
                                 (ac) =>
-                                    Number(ac.price) === Number(price) &&
-                                    ac.assetId ===
-                                        (betOption === EBetOption.YES
-                                            ? currentMarket?.clobTokenIds[0]
-                                            : currentMarket?.clobTokenIds[1])
+                                    Number(ac.price) === Number(price) ||
+                                    Number(ac.price) ===
+                                        Number((1 - Number(price)).toFixed(10))
                             ) ?? null
 
                         return (
